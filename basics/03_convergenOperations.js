d66724259b6d7a5fd747309e146bfc9a6d44edd0
Dtejas2022 <106228821+Dtejas2022@users.noreply.github.com>
2024-02-10 08:41:59 +0000

    conversion od datatypes

---
+++ basics/03_convergenOperations.js
@@ -0,0 +1,37 @@
+let score = null
+let age = undefined
+let name = true
+// console.log(typeof(score)); //type of datatype
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+// console.log(age)
+// console.log(typeof age)
+
+//"33" => 33
+//"33anf" => NaN not a number
+//true => 1
+
+let is_loggedIn = 1
+// let is_loggedIn = 0
+// let is_loggedIn = ""
+// let is_loggedIn = "tejas"
+
+
+let booleanValue = Boolean(is_loggedIn)
+
+console.log(booleanValue);
+
+
+//1 => true; 0 => false
+//"" => false
+//"tejas" => true
+
+
+let number = 14
+
+let stringNum = String(number)
+
+console.log(stringNum)
+console.log(typeof stringNum)
