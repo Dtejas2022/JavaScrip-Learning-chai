62ebde8e3e0218c996ca6f4a146bfbee08273eeb
Dtejas2022 <106228821+Dtejas2022@users.noreply.github.com>
2024-02-10 08:15:19 +0000

    datatypes

---
+++ basics/01_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as new version
+
+//alert(3+3) //we are useing node js not browser
+
+//console.log("hello");console.log("tejas") //code readability should be high
+
+let name = "tejas"
+let age = 23
+let isLoggedIn = false
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value (khali value in container)
+//undefined => value not asign
+//symbol =>  unique
+
+//object
+
+console.log(typeof(undefined));
+console.log(typeof(null));
