1606e02dba662cef91a2a9e188e41dd2521c81f1
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-02-11 10:16:55 +0000

    numbrs and math methods

---
+++ basics/06_numbers_and_math.js
@@ -0,0 +1,33 @@
+const score = 40
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString()); //convert num to string
+console.log(balance.toString().length) //return lenghth
+
+const num2 = 23.189
+console.log(num2.toPrecision(3));
+
+
+
+
+// ==========================Maths======================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+
+console.log(Math.random());  //return 0 to 1 value
+console.log((Math.random()*10) + 1); //more than 1 value
+
+const min = 10
+const max = 20
+
+// console.log();
+
+console.log(Math.floor(Math.random() * (max-min+1) + min))
+
