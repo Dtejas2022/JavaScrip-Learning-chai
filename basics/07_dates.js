d9d3742096a37d2ac970fd6bf28fbe20e64031af
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-02-11 10:39:32 +0000

    date

---
+++ basics/07_dates.js
@@ -0,0 +1,22 @@
+// const mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+// console.log(typeof(mydate))
+
+const myCreatedDate = Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleString());
+
+let mytimestamp = Date.now()
+console.log(mytimestamp);
+
+let newdate = new Date()
+console.log(newdate.getMonth())
+
+
+// newdate.toLocaleString('default',{
+//     weekday: "long",
+//     timeZone: "newdate"
+// })
