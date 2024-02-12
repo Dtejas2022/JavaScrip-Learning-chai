57dc576d7acc8cf0b586f092f4dd2bcbdb9ab3b3
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-02-12 04:38:01 +0000

    objects part2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,66 @@
+//using constructor
+
+//const appUser  = new Object()
+
+const appUser = {}
+
+appUser.id = "123abc"
+appUser.name = "tejas"
+appUser.isLoggedIn = false
+
+
+// console.log(appUser);
+
+const regularUser = {
+    email : 'someone@com',
+    fullname : {
+        userfullname : {
+            fname : 'tejas',
+            lname : 'dotkar'
+        }
+    }
+}
+
+// console.log(regularUser.fullname?.userfullname.lname); //acces values of nested object
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+
+const obj2 = {
+    3:"a",
+    4:"b"
+}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({},obj1,obj2)  //combine two objects
+
+const obj3 = {...obj1, ...obj2}  //eassy way to spred
+console.log(obj3);
+
+
+const users = [
+    {
+        id : 1,
+        email: 't@com'
+    },
+    {
+        id : 1,
+        email: 't@com'
+    },
+    {
+        id : 1,
+        email: 't@com'
+    },
+]
+
+users[1].email
+
+console.log(appUser);
+
+console.log(Object.keys(appUser));
+console.log(Object.values(appUser));
+console.log(Object.entries(appUser));
+
+console.log(appUser.hasOwnProperty('isLoggedIn'));
