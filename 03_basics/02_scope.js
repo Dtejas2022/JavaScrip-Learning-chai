e77498b300079bee90f76931ac136053a58adbaa
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-02-14 05:31:06 +0000

    scope 2

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -8,10 +8,54 @@ if(true){
     const b = 20
     // var c = 30
 
-    console.log(a)
+    // console.log(a)
 
 }
 
 // console.log(a)
 // console.log(b)
- console.log(a)
+//  console.log(a)
+
+function one(){
+    const username = 'tejas'
+
+    function two(){
+        const website = 'youtube'
+        console.log(username)
+    }
+
+    console.log(website);
+    two()
+}
+
+// one()
+
+if(true){
+    const username = 'tejas'
+    if (username ==='tejas'){
+        const website = 'youtube'
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++interesting+++++++++++++++++++++
+
+addone(5)
+
+
+function addone(num){
+    return num+1
+}
+
+// addone(5)
+
+
+// addtwo(5)
+const addtwo = function(num){   //function declaration
+    return num + 2
+}
+
+addtwo(5)
