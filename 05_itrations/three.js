8f8335bc1e8029db1943a5c8e3ed5f1dbbd3c713
Tejas Dotkar <106228821+Dtejas2022@users.noreply.github.com>
2024-02-16 08:48:21 +0000

    forof

---
+++ 05_itrations/three.js
@@ -0,0 +1,18 @@
+// for of
+
+// arrays specific loops
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5,6,7,8,9]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "hello world"
+
+for (const greet of greetings) {
+    console.log(`each char is ${greet}`)
+}
